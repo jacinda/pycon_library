dbdecdf29bf42213b412aeb288b35dea14e67ab3
Jacinda Shelly <jacinda.shelly@gmail.com>
2015-04-07 10:15:40 -0700

    Change the URL used to access the admin. Many automated bots search for /admin, so this improves security.

--- pycon_library/urls.py
+++ pycon_library/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # url(r'^$', 'pycon_library.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^libadmin/', include(admin.site.urls)),
 ]
