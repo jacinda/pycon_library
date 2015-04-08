0ceef124480dab5fbaf57944a1d8e7a52b700d3c
Jacinda Shelly <jacinda.shelly@gmail.com>
2015-04-08 11:36:23 -0400

    Prevent LoanedBookInline from showing up when adding a new user

--- libraryusers/admin.py
+++ libraryusers/admin.py
@@ -13,3 +13,9 @@ class LibraryUserAdmin(UserAdmin):
         return super(LibraryUserAdmin, self).get_fieldsets(request, obj) + (
                 ('Custom Fields', {'fields': ('birthdate', 'gender')}),
         )
+
+    def get_formsets_with_inlines(self, request, obj=None):
+        for inline in self.get_inline_instances(request, obj):
+            if isinstance(inline, LoanedBookInline) and obj is None:
+                continue
+            yield inline.get_formset(request, obj), inline
