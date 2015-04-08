03015452e614b4dc506512f13370afcd8db639f5
Jacinda Shelly <jacinda.shelly@gmail.com>
2015-04-07 23:20:35 -0400

    Example of creating a custom AdminSite based on Django docs

---
+++ pycon_library/admin.py
@@ -0,0 +1,11 @@
+# This is actually pulled almost straight from the Django Admin documentation.
+# https://docs.djangoproject.com/en/1.8/ref/contrib/admin/#customizing-the-adminsite-class
+# 
+#from django.contrib.admin import AdminSite
+#
+#class MyAdminSite(AdminSite):
+#    site_header = 'Monty Python administration'
+#    site_title = 'Monty Python administration'
+#    index_title = 'Monty Python administration'
+#
+##admin_site = MyAdminSite(name='myadmin')
