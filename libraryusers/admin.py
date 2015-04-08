6ebe6fb0f9bb289d3e3e259770cc888b06a0ad2f
Jacinda Shelly <jacinda.shelly@gmail.com>
2015-04-07 23:59:47 -0400

    Use 1.7+ register decorator to register the LibraryUser model

--- libraryusers/admin.py
+++ libraryusers/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import LibraryUser
 
+@admin.register(LibraryUser)
 class LibraryUserAdmin(UserAdmin):
     pass
-
-admin.site.register(LibraryUser, LibraryUserAdmin)
