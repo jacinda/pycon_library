fc8c566df73f8bbc00dfebbb4a430ac38e51de38
Jacinda Shelly <jacinda.shelly@gmail.com>
2015-04-07 23:35:53 -0400

    Change Admin text the easy way; remove base_site.html template

--- pycon_library/urls.py
+++ pycon_library/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy
 
 urlpatterns = [
     # Examples:
@@ -8,3 +9,10 @@ urlpatterns = [
 
     url(r'^libadmin/', include(admin.site.urls)),
 ]
+
+# Change Admin Site Title
+admin.site.site_title = ugettext_lazy('PyCon Library Admin')
+# Change the <h1> for the Admin
+admin.site.site_header = ugettext_lazy('PyCon Library Administration')
+# Text to put at the top of the admin index page.
+admin.site.index_title = ugettext_lazy('PyCon Library Administration')
